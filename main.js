c6efc0ccaca47993bf390e9804a57b9841d4f4b3
Erick <ericksantosdep@gmail.com>
2026-06-17 03:06:09 -0300

    Detalhando o Java Script

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
-const display = document.getElementById("display");
-const botoes = document.querySelectorAll("button");
-botoes.forEach((botao) => {
-  botao.addEventListener("click", () => {
-    const valor = botao.textContent;
+const display = document.getElementById("display"); //Adicionando o #display
+const botoes = document.querySelectorAll("button"); //Selecionando todos os butões
+botoes.forEach((botao) => {                         //Passa por cada item da lista...
+  botao.addEventListener("click", () => {           //Quando o "click" acontecer faça tal função
+    const valor = botao.textContent;                //Pega o texto dentro do elemento
     if (valor === "C") {
       display.value = "";
     } else if (valor === "=") {
-      try {
-        display.value = eval(display.value);
-      } catch {
+      try {                                         //Tente executar esse código
+        display.value = eval(display.value);            //Calcula o valor no display
+      } catch {                                     //Caso não consiga executar
         display.value = "Erro";
       }
     } else {
-      display.value += valor;
+      display.value += valor;                       //Se o botão não for "C" ou "=" adicione o valor
     }
   });
 });
